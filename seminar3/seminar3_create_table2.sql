354058635d5b691c99082cba54c3c0dc2b0ac93d
Nikolay Eremeev <nikoeremeev@gmail.com>
2022-10-11 19:41:02 +0300

    Add table2

--- seminar3/seminar3_create_table2.sql
+++ seminar3/seminar3_create_table2.sql
@@ -2,17 +2,20 @@ USE gb_seminar3;
 
 CREATE TABLE customers
 (
-snum INT PRIMARY KEY UNIQUE,
+cnum INT PRIMARY KEY UNIQUE,
 cname CHAR(10) NOT NULL,
 city CHAR(10) NOT NULL,
 rating INT NOT NULL,
+snum INT,
 FOREIGN KEY (snum)  REFERENCES salespeople (snum)
 );
 
-INSERT INTO salespeople(snum, sname, city, comm)
+INSERT INTO customers(cnum, cname, city, rating, snum)
 VALUES
-(1001, "Peel", "London", 0.12),
-(1002, "Serres", "San Jose", 0.13),
-(1004, "Motika", "London", 0.11),
-(1007, "Rifkin", "Barcelona", 0.15),
-(1003, "Axelrod", "New York", 0.10);
+(2001, "Hoffman", "London", 100, 1001),
+(2002, "Giovanni", "Rome", 200, 1003),
+(2003, "Liu", "SanJose", 200, 1002),
+(2004, "Grass", "Berlin", 300, 1002),
+(2006, "Clemens", "London", 100, 1001),
+(2008, "Cisneros", "SanJose", 300, 1007),
+(2007, "Pereira", "Rome", 100, 1004);
